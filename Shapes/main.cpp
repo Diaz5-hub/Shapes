622cd97211e21aa9c2a7c998ced36421ee9d7c8d
Diaz5-hub <81527723+Diaz5-hub@users.noreply.github.com>
2021-03-28 11:05:10 -0700

    Initial Commit

---
+++ Shapes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Shapes
+//
+//  Created by Alexander Diaz on 3/28/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
